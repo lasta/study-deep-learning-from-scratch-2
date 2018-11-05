caf8cf314cfcd97e21cb7805dc128227d5e2e2e5
Atsushi KOSHIKIZAWA <atsushi-koshikizawa@navitime.co.jp>
2018-11-05 20:27:05 +0900

    end of chapter 2

--- ch02/count_method_small.py
+++ ch02/count_method_small.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('..')
 import numpy as np
 import matplotlib
-matplotlib.use('Agg')
+matplotlib.use('WXAgg')
 import matplotlib.pyplot as plt
 from common.util import preprocess, create_co_matrix, ppmi
 
@@ -17,12 +17,19 @@ W = ppmi(C)
 # SVD; W ~ U * S * Vt
 U, S, V = np.linalg.svd(W)
 
-print(corpus)
-print(word_to_id)
-print(id_to_word)
-print('co-occurance matrix')
-print(C[0])
-print('PPMI matrix')
-print(W[0])
-print('SVD')
-print(U[0])
+# print(corpus)
+# print(word_to_id)
+# print(id_to_word)
+# print('co-occurance matrix')
+# print(C[0])
+# print('PPMI matrix')
+# print(W[0])
+# print('SVD')
+# print(U[0])
+# print(U[0, :2])
+
+for word, word_id in word_to_id.items():
+    plt.annotate(word, (U[word_id, 0], U[word_id, 1]))
+
+plt.scatter(U[:,0], U[:,1], alpha=0.5)
+plt.show()
